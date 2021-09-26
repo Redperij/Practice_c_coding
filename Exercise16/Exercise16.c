799c84bc38b4d82419cf276b61d6d8865d921b4d
daniilm <Daniil.Marinec@metropolia.fi>
2021-09-26 22:16:14 +0300

    Empty Exercise16.c

---
+++ Exercise16/Exercise16.c
@@ -0,0 +1,14 @@
+#pragma warning(disable:4996) //disabling warning
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_FILENAME 255
+
+int main() {
+
+	return 0;
+}
