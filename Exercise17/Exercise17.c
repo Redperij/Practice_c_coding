c40e4b328a0d10cc63661b5cea4361c4ea4780b5
daniilm <Daniil.Marinec@metropolia.fi>
2021-09-30 17:38:09 +0300

    Empty Exercise17

---
+++ Exercise17/Exercise17.c
@@ -0,0 +1,12 @@
+#pragma warning(disable:4996) //disabling warning
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+
+	return 0;
+}
