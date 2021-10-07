1be29ec25b84945d566984699654fa792b47d460
daniilm <Daniil.Marinec@metropolia.fi>
2021-10-07 20:19:55 +0300

    Exercise20 - done

--- Exercise20/Exercise20.c
+++ Exercise20/Exercise20.c
@@ -11,7 +11,9 @@
 #define MAX_PRINTABLE_CHAR 127
 #define PASSWORDS_TO_GENERATE 3
 
-void gen_password(char *password, unsigned int array_size, const char **array_of_words);
+//Generates random password with one of the words from array_of_words of array_size elements.
+//Returns generated password into *password.
+void gen_password(char **password, unsigned int array_size, const char **array_of_words);
 //Fills given char array of 'size' size with random printable non-whitespace characters and terminates it in the end.
 void random_prstring(char *array, size_t size);
 //Returns random number. min < x <= max
@@ -21,26 +23,35 @@ int main(void)
 {
 	char *generated_password = NULL;
 	char **array_of_words = malloc(WORDS * sizeof(char *));
-	if (array_of_words == NULL) return 1;
+	if (array_of_words == NULL) return 1; //malloc() failed -> escape.
+
+	srand(time(NULL)); //Current time is seed for rand() function.
+	
+	//Filling in array of strings.
 	array_of_words[0] = "Hello";
 	array_of_words[1] = "Again";
-	array_of_words[2] = "SQDX";
+	array_of_words[2] = "Cat";
 	array_of_words[3] = "Track";
 	array_of_words[4] = "Road";
-	array_of_words[5] = "Hello";
-	array_of_words[6] = "Again";
-	array_of_words[7] = "SQDX";
-	array_of_words[8] = "Track";
-	array_of_words[9] = "Road";
+	array_of_words[5] = "Hammer";
+	array_of_words[6] = "Tree";
+	array_of_words[7] = "CQDX";
+	array_of_words[8] = "Apple";
+	array_of_words[9] = "Pie";
 
-	srand(time(NULL));
-	gen_password(generated_password, WORDS, array_of_words);
+	//Generating 'PASSWORDS_TO_GENERATE' passwords.
+	for (int i = 0; i < PASSWORDS_TO_GENERATE; i++) {
+		gen_password(&generated_password, WORDS, array_of_words);
 
-	//Free memory.
-	if (generated_password != NULL) {
-		free(generated_password);
-		generated_password = NULL;
+		printf("Password %d: %s\n", i + 1, generated_password);
+		//Free memory.
+		if (generated_password != NULL) {
+			free(generated_password);
+			generated_password = NULL;
+		}
 	}
+	
+	//Free memory.
 	if (array_of_words != NULL) {
 		free(array_of_words);
 		array_of_words = NULL;
@@ -48,16 +59,18 @@ int main(void)
 	return 0;
 }
 
-void gen_password(char *password, unsigned int array_size, const char **array_of_words) {
+//Generates random password with one of the words from array_of_words of array_size elements.
+//Returns generated password into *password.
+void gen_password(char **password, unsigned int array_size, const char **array_of_words) {
 	if (array_of_words == NULL) return; //array is empty -> escape.
 	unsigned int rnd_chars = 0; //Random chars to add to the word.
 	unsigned int word = 0; //Which word was randomly selected.
 	size_t password_size = 0; //Size of the password.
 
 	//Password must be empty.
-	if (password != NULL) {
-		free(password);
-		password = NULL;
+	if (*password != NULL) {
+		free(*password);
+		*password = NULL;
 	}
 	//Getting number of chars to add.
 	rnd_chars = get_rand_num(0, MAX_CHARS_TO_APPEND);
@@ -67,25 +80,19 @@ void gen_password(char *password, unsigned int array_size, const char **array_of
 	password_size = strlen(array_of_words[word]) + rnd_chars;
 
 	//Allocating memory for the password.
-	password = (char *)malloc((password_size + 1) * sizeof(char));
+	*password = (char *)malloc((password_size + 1) * sizeof(char));
 	
-	if (password != NULL) {
+	if (*password != NULL) {
 		//Choosing place where to add word.
-		int place_to_add_word = get_rand_num(0, password_size - strlen(array_of_words[word]) ) - 1;
+		int place_to_add_word = get_rand_num(0, password_size - strlen(array_of_words[word]) + 1 ) - 1;
 		int q = 0;
 		//Filling password with characters and terminating it.
-		random_prstring(password, password_size);
-		for (int i = place_to_add_word; i < strlen(array_of_words[word]); i++) {
-			password[i] = array_of_words[word][q];
+		random_prstring(*password, password_size);
+		//Placing word into the string overwriting random characters.
+		for (int i = place_to_add_word; i < place_to_add_word + strlen(array_of_words[word]); i++) {
+			password[0][i] = array_of_words[word][q];
 			q++;
 		}
-		//Loop is incorrect.
-
-
-		printf("Password: %s\n", password);
-		printf("Chosen word: %s\n", array_of_words[word]);
-		printf("Number of chars to add: %d\n", rnd_chars);
-		printf("Place to add word: %d\n", place_to_add_word);
 	}
 }
 
@@ -96,7 +103,7 @@ void random_prstring (char *array, size_t size) {
 	while (i < size) {
 		char_to_append = get_rand_num(0, MAX_PRINTABLE_CHAR);
 		if (isprint(char_to_append) && !isspace(char_to_append)) {
-			array[i] = char_to_append;
+			array[i] = char_to_append; 
 			i++;
 		}
 	}
