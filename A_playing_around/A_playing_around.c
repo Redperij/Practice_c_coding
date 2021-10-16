f452e98ae676a8dfa1f2e3a04bf5fb59e6ad8064
daniilm <Daniil.Marinec@metropolia.fi>
2021-10-16 09:55:01 +0300

    Ville 6.2

--- A_playing_around/A_playing_around.c
+++ A_playing_around/A_playing_around.c
@@ -1,10 +1,13 @@
 #pragma warning(disable:4996) //disabling warning
 
 /*
-Write implementation of function printd that takes same parameters as printf and produces same output as printf but precedes all output with "DEBUG: ".
-Function returns number of output characters including the debug prefix in the output.
+Implement two functions:
 
-For example, if you call printd("test") then output is "DEBUG: test" and return value is 11.
+int log(loglevel level, const char *format, ...); works like printf but prints only when level lower than or equal to global variable log_level.
+When printing precedes printouts with "LOG[%s]: " where %s is the parameter level printed as a string.
+
+const char *log_level_to_str(loglevel level); returns a constant string corresponding to the level parameter.
+Note that the string returned may not be a variable that is allocated on the stack.
 */
 
 #include <stdio.h>
@@ -14,24 +17,48 @@ For example, if you call printd("test") then output is "DEBUG: test" and return 
 #include <ctype.h>
 #include <stdbool.h>
 
-int printd(const char *format, ...);
+typedef enum { logCritical, logWarning, logInfo, logVerbose } loglevel;
+// Log levels as strings:  "Critical", "Warning", "Info", "Verbose" 
+
+const char *log_level_to_str(loglevel level);
+// implement this function
+int log(loglevel level, const char *format, ...);
+
+loglevel log_level = logCritical;
 
 int main(int arcg, char **argv)
 {
-    printd("Holy why?\n");
+	log(logCritical, "OH NO\n");
+	log(logInfo, "OH NOx2\n");
     return 0;
-
 }
 
-int printd(const char *format, ...) {
-    va_list argptr;
-    int cnt = 0; //count
+const char *log_level_to_str(loglevel level) {
+	switch (level)
+	{
+	case logCritical:
+		return "Critical";
+	case logWarning:
+		return "Warning";
+	case logInfo:
+		return "Info";
+	case logVerbose:
+		return "Verbose";
+	default:
+		return NULL;
+	}
+}
+int log(loglevel level, const char *format, ...) {
+	va_list argptr; //List with arguments.
+	int cnt = 0; //Count printed characters.
 
-    va_start(argptr, format);
-    //Printing message.
-    cnt += printf("DEBUG: ");
-    cnt += vprintf(format, argptr);
-    va_end(argptr);
-    
-    return cnt;
+	if (level <= log_level) {
+		va_start(argptr, format);
+		//Printing message.
+		cnt += printf("LOG[%s]: ", log_level_to_str(level));
+		cnt += vprintf(format, argptr);
+		va_end(argptr);
+	}
+
+	return cnt;
 }
